857a8ff4bdb27c49a82475e2f6e23774764eb9c5
Sachin C Kasar <cse.sck@gmail.com>
2018-08-06 16:15:33 +0530

    Common Eureka and Config Server useful for other sessions in this training repository.
    Common Eureka and Config Server useful for other sessions in this training repository.

---
+++ common/config-server/src/test/java/demo/OutOfContainerTest.java
@@ -0,0 +1,40 @@
+package demo;
+
+import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+@WebAppConfiguration
+@ActiveProfiles("native")	//	"native" means use local classpath location rather than GitHub.
+public class OutOfContainerTest {
+
+	@Autowired WebApplicationContext spring;
+	MockMvc mockMvc;
+	
+	@Before
+	public void setup() {
+		 
+	}
+	
+	@Test
+	public void propertyLoadTest() throws Exception {
+
+		 
+	}
+}
